c17c3df579b6e6659ad9b6f8c2504d3f2f7f3fb0
davidepatruno <patrunodavid@gmail.com>
2018-12-12 17:23:05 +0100

    Complete

--- main.js
+++ main.js
@@ -6,13 +6,42 @@ $(document).ready(function(){
       var numero_inserito = thisinput.val();
       console.log(numero_inserito);
       $.ajax({
-        url: "www.boolean.careers/api/array/basket",
+        url: "https://www.boolean.careers/api/array/basket",
         method: "GET",
         data: {
           n: numero_inserito
         },
         success: function(data){
           console.log(data);
+          for (var i=0; i < data.response.length; i++){
+            giocatori.push(data.response[i]);
+          }
+          for (var i = 0; i < giocatori.length; i++) {
+            var cod_giocatore = giocatori[i].playerCode;
+
+            console.log(giocatori[i].playerCode);
+            var source = $('#playercode_template').html();
+             //document.getElementById("playercode_template").innerHTML;
+            var template = Handlebars.compile(source);
+            var context = {codicegiocatore : cod_giocatore};
+            var html    = template(context);
+            $(".codici_player").append(html);
+          }
+          $('.playercode').click(function(){
+            var thisCode = $(this).text();
+            for (var i = 0; i < giocatori.length; i++) {
+              var cod_giocatore = giocatori[i].playerCode;
+              if (thisCode == cod_giocatore) {
+                console.log(giocatori[i]);
+                var source = $('#stats_template').html();
+                 //document.getElementById("playercode_template").innerHTML;
+                var template = Handlebars.compile(source);
+                var context = {code : giocatori[i].playerCode, bounce : giocatori[i].rebounds, score : giocatori[i].points, fault : giocatori[i].fouls, shoots2 : giocatori[i].twoPoints, shoots3 : giocatori[i].threePoints };
+                var html    = template(context);
+                $(".stats_wrapper").html(html);
+              }
+            }
+          });
         },
         error: function(){
           alert("non funzia");
@@ -21,72 +50,3 @@ $(document).ready(function(){
     }
   });
 });
-
-
-
-//   for (var i = 0; i < giocatori.length; i++) {
-//     var playercode = giocatori[i].codice_Giocatore;
-//     // var divgiocatore = '<div class="playercode">' + codicegiocatore + '</div>';
-//
-//     // console.log("scrivo l'oggetto");
-//     // console.log(giocatori[i].codice_Giocatore);
-//     var source = $('#playercode_template').html();
-//      //document.getElementById("playercode_template").innerHTML;
-//     var template = Handlebars.compile(source);
-//     var context = {codicegiocatore : playercode};
-//     var html    = template(context);
-//     $(".codici_player").append(html);
-//   }
-//   $('.playercode').click(function(){
-//     var thisCode = $(this).text();
-//     for (var i = 0; i < giocatori.length; i++) {
-//       var playercode = giocatori[i].codice_Giocatore;
-//       if (thisCode == playercode) {
-//         console.log(giocatori[i]);
-//         var source = $('#stats_template').html();
-//          //document.getElementById("playercode_template").innerHTML;
-//         var template = Handlebars.compile(source);
-//         var context = {code : giocatori[i].codice_Giocatore, bounce : giocatori[i].num_Rimbalzi, score : giocatori[i].punti_Segnati, fault : giocatori[i].falli, shoots2 : giocatori[i].successo_duePunti, shoots3 : giocatori[i].successo_trePunti };
-//         var html    = template(context);
-//         $(".stats_wrapper").html(html);
-//       }
-//     }
-//     console.log($(this).text());
-//   })
-// })
-//
-//
-//
-// function creaCodice(){
-//     var codePlayer = "";
-//     var casualNumber = "";
-//
-//     for (var i = 0; i < 3; i++) {
-//       //Genero una lettera casuale maiuscola prendendo i valori dall'unicode
-//       var casualLetter = String.fromCharCode(Math.floor((Math.random()*(90 - 65 + 1) + 65)));
-//       codePlayer += casualLetter;
-//     }
-//
-//     for (var i = 0; i < 3; i++) {
-//       casualNumber = Math.floor((Math.random() * 9) + 1);
-//       codePlayer += casualNumber;
-//     }
-//
-//     return codePlayer;
-// }
-
-
-
-
-
-
-// var codgiocatoretemp = $('.template .giocatore_template').clone();
-// codgiocatoretemp.text(codiceGiocatore);
-// $('.giocatore').append(codgiocatoretemp);
-//
-// var templatescheda = $('.template .template_scheda').clone();
-// var scheda_da_inserire = $('.template .template_scheda .scheda_giocatore').clone();
-// scheda_da_inserire.children('.punti_Segnati').text(puntiFatti);
-// scheda_da_inserire.children('.num_rimbalzi').text(rimbalzi);
-// // $('.scheda_giocatore').append(scheda_da_inserire);
-// $('.schede_wrapper').append(templatescheda);
